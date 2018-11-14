0e82b953f88d87d667ce81efb23b3f257ecb3aca
Luis Reyes <larcman@gmail.com>
2018-11-14 13:42:44 -0600

    Grid design & Messages enhancements

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,25 @@
 class SessionsController < ApplicationController
   def new
-    
+    if logged_in?
+      redirect_to root_path
+    end
   end
 
   def create
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:success] = "You have successfully logged in"
+      # flash[:success] = "You have successfully logged in"
       redirect_to root_path
     else
-      flash.now[:danger] = "There was something wrong with your login information"
+      flash.now[:danger] = "El email o contraseña son incorrectos"
       render 'new'
     end
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:success] = "You have successfully logged out"
+    # flash[:success] = "You have successfully logged out"
     render 'new'
   end
 end
