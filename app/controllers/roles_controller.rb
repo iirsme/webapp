f09df34fe34572c8f85f80c55d4a9c7a12ae2bb9
Luis Reyes <larcman@gmail.com>
2018-11-15 19:42:12 -0600

    destroy&index actions in Roles

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,8 +1,14 @@
 class RolesController < ApplicationController
+  before_action :set_current_view
+  before_action :set_role, only: [:edit, :update, :show, :destroy]
   before_action :require_user
 
   def index
-    @current_view = 'roles'
+    @roles = Role.all
+  end
+  
+  def new
+    @role = Role.new
   end
   
   def create
@@ -10,11 +16,12 @@ class RolesController < ApplicationController
   end
   
   def show
-    
   end
   
   def destroy
-    
+    @role.destroy
+    flash[:danger] = "Rol borrado satisfactoriamente"
+    redirect_to roles_path
   end
   
   def edit
@@ -23,4 +30,12 @@ class RolesController < ApplicationController
   def update
     
   end
+  
+  private
+    def set_current_view
+      @current_view = 'roles'
+    end
+    def set_role
+      @role = Role.find(params[:id])
+    end
 end
