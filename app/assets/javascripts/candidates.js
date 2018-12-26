b4a36c5f8e7acbfcfab310f761816b50018991be
Luis Reyes <larcman@gmail.com>
2018-12-26 15:37:09 -0600

    Enhanced WS combos code

--- app/assets/javascripts/candidates.js
+++ app/assets/javascripts/candidates.js
@@ -53,16 +53,32 @@ $(document).on('ready turbolinks:load', function () {
     candidateFormLogic();
   });
   $('.candidate-bcountry-field').change(function () {
-  	// TODO: Clean states field
-    getStates(true);
+  	$('.candidate-bstate-field').val("");
+  	$('.candidate-bstate-field').empty();
+  	$('.candidate-bcity-field').val("");
+  	$('.candidate-bcity-field').empty();
+    getBirthdayStates(true);
   });
   $('.candidate-bstate-field').change(function () {
-  	// TODO: Clean cities field
-    getCities(true);
+  	$('.candidate-bcity-field').val("");
+  	$('.candidate-bcity-field').empty();  	
+    getBirthdayCities(true);
+  });
+  $('.candidate-acountry-field').change(function () {
+  	$('.candidate-astate-field').val("");
+    $('.candidate-astate-field').empty();
+    $('.candidate-acity-field').val("");
+  	$('.candidate-acity-field').empty();
+    getAddressStates(true);
+  });
+  $('.candidate-astate-field').change(function () {
+    $('.candidate-acity-field').val("");
+  	$('.candidate-acity-field').empty();
+    getAddressCities(true);
   });
 });
 
-candidateFormLogic = function () {
+function candidateFormLogic () {
   var occupation = $('.candidate-occupation-field').val();
   if (occupation === 'Otro') {
     $('.candidate-other-occupation-field').show();
@@ -71,20 +87,28 @@ candidateFormLogic = function () {
   }
 };
 
-function getCombosData () {
-  if ($('#is-new-candidate').val() === "true") {
-    getCountries();
-  } else {
-  	getCountries();
-    getStates();
-    getCities();
+function getInitialCombosData () {
+  if ($('#request_sent').val() == "false") {
+    if ($('#is-new-candidate').val() === "true") {
+      getCountries();
+    } else {
+      getCountries();
+      getStates();
+      getCities();
+    }
+    $('#request_sent').val(true);
   }
 };
 
-function getCountries (showLoading) { console.log("Calling AJAX 1...");
-  if (showLoading) {
-    $('.birth-country-loading').show();
-  }
+function getCountries () { console.log("Loading countries...");
+  var values = {};
+  var aCountry = $(".candidate-acountry-field");
+  var ahCountry = $("#candidate-acountry-field");
+  var bCountry = $(".candidate-bcountry-field");
+  var bhCountry = $('#candidate-bcountry-field');
+
+  aCountry.empty();
+  bCountry.empty();
   $.ajax({
     type: "GET",
     url: "http://api.geonames.org/countryInfoJSON?lang=es&formatted=true&style=full&username=sanjish",
@@ -92,45 +116,68 @@ function getCountries (showLoading) { console.log("Calling AJAX 1...");
     dataType: "jsonp",
     success: function (data) {
       $(data.geonames).each(function (index, item) {
-        $(".candidate-bcountry-field").append($('<option />', { value: item.geonameId, text: item.countryName }));
+      	values = { value: item.geonameId, text: item.countryName };
+        $(bCountry).append($('<option />', values));
+        $(aCountry).append($('<option />', values));
       });
-      var currentValue = $('#birth-country').val();
-      $(".candidate-bcountry-field").val(currentValue);
-      $(".candidate-bcountry-field").slideDown();
-      if (showLoading) {
-        $('.birth-country-loading').hide();
-      }
+      $(bCountry).val(bhCountry.val());
+      $(aCountry).val(ahCountry.val());
     },
     error: function (data) { debugger;
-      if (showLoading) {
-        $('.birth-country-loading').hide();
-      }
       alert("Error al obtener lista de paises");
     }
   });
 }
 
-function getStates (showLoading) { console.log("Calling AJAX 2...");
-  var country = $(".candidate-bcountry-field").val() || $('#birth-country').val();
-  if (country) {
-    getChildren(country, $('.candidate-bstate-field'), $('#birth-state'), showLoading, $('.birth-state-loading'));
+function getBirthdayStates (showLoading) {
+  var country = $(".candidate-bcountry-field").val() || $('#candidate-bcountry-field').val();
+  if (country && country !== "") {
+  	console.log("Loading birthday states...");
+    getChildren(country, $('.candidate-bstate-field'), $('#candidate-bstate-field'), showLoading, $('.candidate-bstate-loading'));
   }
 }
 
-function getCities (showLoading) { console.log("Calling AJAX 3...");
-  var state = $(".candidate-bstate-field").val() || $('#birth-state').val();
-  if (state) {
-    getChildren(state, $('.candidate-bcity-field'), $('#birth-city'), showLoading, $('.birth-city-loading'));
+function getBirthdayCities (showLoading) {
+  var state = $(".candidate-bstate-field").val() || $('#candidate-bstate-field').val();
+  if (state && state !== "") { 
+  	console.log("Loading birthday cities...");
+    getChildren(state, $('.candidate-bcity-field'), $('#candidate-bcity-field'), showLoading, $('.candidate-bcity-loading'));
   }
 };
 
-function getChildren (parent, field, hiddenField, showLoading, indicator) {
+function getAddressStates (showLoading) {
+  var country = $(".candidate-acountry-field").val() || $('#candidate-acountry-field').val();
+  if (country && country !== "") {
+  	console.log("Loading address states...");
+    getChildren(country, $('.candidate-astate-field'), $('#candidate-astate-field'), showLoading, $('.candidate-astate-loading'));
+  }
+}
+
+function getAddressCities (showLoading) {
+  var state = $(".candidate-astate-field").val() || $('#candidate-astate-field').val();
+  if (state && state !== "") {
+    console.log("Loading address cities...");
+    getChildren(state, $('.candidate-acity-field'), $('#candidate-acity-field'), showLoading, $('.candidate-acity-loading'));
+  }
+};
+
+function getStates () {
+  getBirthdayStates();
+  getAddressStates();
+}
+
+function getCities () {
+  getBirthdayCities();
+  getAddressCities();
+}
+
+function getChildren (parentId, field, hiddenField, showLoading, indicator) {
   if (showLoading) {
     $(indicator).show();
   }	
   $.ajax({
     type: "GET",
-    url: "http://api.geonames.org/childrenJSON?geonameId=" + parent + "&username=sanjish&lang=es",
+    url: "http://api.geonames.org/childrenJSON?geonameId=" + parentId + "&username=sanjish&lang=es",
     contentType: "application/json; charset=utf-8",
     dataType: "jsonp",
     success: function (data) {
