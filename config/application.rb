bfacb37e84c9e01d6b3b3f7c5e48be13af609e04
Luis Reyes <larcman@gmail.com>
2019-01-17 01:12:33 -0600

    Added fonts and minor changes in dashboard

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module IirsmeApp
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
     config.i18n.available_locales = %i(en es)
     config.i18n.default_locale = :es
+    
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
   end
 end
