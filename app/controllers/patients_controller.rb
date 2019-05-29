a97f7e4c7b7a5a394cd19de54fbdb3192cf6f220
Luis Reyes <larcman@gmail.com>
2019-05-29 00:18:02 -0500

    Patients (destroy)

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,9 +2,36 @@ class PatientsController < ApplicationController
   before_action :set_current_view
   before_action :set_patient, only: [:edit, :update, :show, :destroy]
   before_action :set_current_research
+  before_action only: [:edit, :update, :show, :destroy] do
+    require_research_user(@current_research)
+  end
 
   def index
     @patients = Patient.where(research_id: @current_research.id)
+  end
+
+  def new
+    @patient = Patient.new
+  end
+
+  def create
+    @patient = Patient.new(patient_params)
+    @patient.research = @current_research
+    if @patient.save
+      flash[:success] = "Paciente agregado al protocolo satisfactoriamente"
+      redirect_to edit_patient_path(@patient)
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @patient.destroy
+    flash[:success] = "Paciente eliminado satisfactoriamente del protocolo"
+    redirect_to patients_path(research_id: @current_research)
+  end
+
+  def edit
   end
 
   private
@@ -12,6 +39,7 @@ class PatientsController < ApplicationController
     @current_view = 'patients'
   end
   def set_current_research
+    puts "******* #{params}"
     @current_research = Research.find(params[:research_id])
   end
   def set_patient
