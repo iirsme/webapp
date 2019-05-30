49c0ab325b47272021db6f2e33d25a5574321d0b
Luis Reyes <larcman@gmail.com>
2019-05-30 11:28:40 -0500

    Added require_research_user validation in EvaluationsController

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -2,6 +2,9 @@ class EvaluationsController < ApplicationController
   before_action :set_current_view
   before_action :set_current_research
   before_action :set_appointment, only: [:show]
+  before_action only: [:show] do
+    require_research_user(@current_research)
+  end
 
   def show
   end
