9b1e1c6e6efe6ed8a6fc5b9d87d2139538e98db1
Luis Reyes <larcman@gmail.com>
2019-06-18 21:53:24 -0500

    Added new MultiSelect

---
+++ app/assets/javascripts/evaluations.js
@@ -0,0 +1,16 @@
+$(document).on('ready turbolinks:load', function () {
+  $('.multi_select').multiselect({
+    buttonWidth: '100%',
+    buttonClass: 'form-control',
+    includeSelectAllOption: true,
+    maxHeight: 350,
+    disableIfEmpty: true,
+    selectAllText: 'Seleccionar todo',
+    nonSelectedText: 'Ninguno selecionado...',
+    allSelectedText: 'Todos seleccionados...',
+    numberDisplayed: 0,
+    templates: {
+      ul: '<ul class="multiselect-container col-xs-12 col-md-12 dropdown-menu" style="max-height: 350px; overflow: hidden auto;"></ul>'
+    }
+  });
+});
